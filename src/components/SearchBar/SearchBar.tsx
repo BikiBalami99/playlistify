930cdef0575538f5a58b032e67d9ff5c84f6d00c
Biki Balami <bikibalami99@gmail.com>
2024-09-18 20:37:48 +0900

    Implement feature: Search button removal, search happens as we type

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -13,8 +13,7 @@ const SearchBar = ({ setSearchResults, allTracks }: SearchBarProps) => {
     setSearchTerm(() => e.target.value);
   }
 
-  function handleSearchAction(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+  function filterTracks() {
     if (searchTerm === "") {
       setSearchResults([]);
     } else {
@@ -36,17 +35,44 @@ const SearchBar = ({ setSearchResults, allTracks }: SearchBarProps) => {
     }
   }
 
+  useEffect(() => filterTracks(), [searchTerm]);
+
   return (
-    <form onSubmit={handleSearchAction}>
+    <form>
       <input
         type="text"
         placeholder="Search"
         value={searchTerm}
         onChange={handleSearchInputChange}
       />
-      <button type="submit">Search</button>
     </form>
   );
 };
 
 export default SearchBar;
+
+
+// old handle function if you need
+
+ // function handleSearchAction(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault();
+  //   if (searchTerm === "") {
+  //     setSearchResults([]);
+  //   } else {
+  //     const searchTermLC = searchTerm.toLowerCase();
+  //     setSearchResults(
+  //       allTracks.filter((track) => {
+  //         const trackNameLC = track.name.toLowerCase();
+  //         const albumNameLC = track.album.toLowerCase();
+  //         const artistNameLC = track.artist.toLowerCase();
+  //         if (trackNameLC.includes(searchTermLC)) {
+  //           return true;
+  //         } else if (artistNameLC.includes(searchTermLC)) {
+  //           return true;
+  //         } else if (albumNameLC.includes(searchTermLC)) {
+  //           return true;
+  //         }
+  //       })
+  //     );
+  //   }
+  // }
